e3d013081deacb017634ea4f056a524f6c54c973
unknown <atikurahad@gmail.com>
2024-12-17 23:43:01 +0600

    App

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 
-
+app.get("/",(req,res)=>{
+    res.send("Welcome to App")
+})
 
 app.listen(8080,()=>{
     console.log("App is listening");
